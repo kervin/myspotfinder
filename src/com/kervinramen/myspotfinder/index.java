698e7c48e131e9a3dafd723d7a1af9a212873ad2
kevinachoolhun <kevina.choolhun@gmail.com>
2011-08-14 17:08:10 +0400

    Signed-off-by: kevinachoolhun <kevina.choolhun@gmail.com>

--- src/com/kervinramen/myspotfinder/index.java
+++ src/com/kervinramen/myspotfinder/index.java
@@ -1,13 +1,37 @@
 package com.kervinramen.myspotfinder;
 
-import android.app.Activity;
-import android.os.Bundle;
 
-public class index extends Activity {
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+public class index extends ListActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        listPlaces();
+    }
+    
+    @Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		
+		//on click, open Google Maps Activity
+		Intent i = new Intent(this,  placesmap.class);
+		startActivity(i);
+	}
+    
+    
+    private void listPlaces()
+    {
+    	
+    	//getPlaces, display in list view
+    	
+		//SimpleCursorAdapter friends = new SimpleCursorAdapter(this, R.layout.place_row, notesCursor, from, to);
+    	//setListAdapter();
     }
 }
