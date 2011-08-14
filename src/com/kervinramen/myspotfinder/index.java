540e28e20d1225d16a48e369bd23f78c1712038c
kevinachoolhun <kevina.choolhun@gmail.com>
2011-08-14 23:28:12 +0400

    committing Signed-off-by: kevinachoolhun <kevina.choolhun@gmail.com>

--- src/com/kervinramen/myspotfinder/index.java
+++ src/com/kervinramen/myspotfinder/index.java
@@ -2,51 +2,43 @@ package com.kervinramen.myspotfinder;
 
 
 
-
-import com.kervinramen.myspotfinder.model.Spots;
-
-import android.app.ListActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class index extends ListActivity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        listPlaces();
-    }
-    
-    @Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		super.onListItemClick(l, v, position, id);
+public class index extends Activity{
+
+	EditText txtUsername;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.index);
 		
-		//on click, open Google Maps Activity
-		Intent i = new Intent(this,  placesmap.class);
+		txtUsername =(EditText)findViewById(R.id.userName);
+		
+		Button enterButton = (Button) findViewById(R.id.enter);
+		
+		enterButton.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				showListActivity();
+				
+			}
+		});
+	}
+	
+	private void showListActivity()
+	{
+		Intent i = new Intent(this, placeslist.class);
+		i.putExtra(placeslist.KEY_USERNAME, txtUsername.getText());
+		
 		startActivity(i);
 	}
-    
-    
-    private void listPlaces()
-    {
-    	//getPlaces,
-    	Spots spots = Spots.getSpotsFromWS();
-     
-    	String[] spotNames = new String[spots.getSpots().size()];
-    	
-    	
-    	for (int i = 0; i < spots.getSpots().size(); i++)
-    	{
-    		spotNames[i] = spots.getSpots().get(i).getName();
-    	}
-    	
-       	//display in list view 	
-    	setListAdapter(new ArrayAdapter<String>(this, R.layout.place_row,
-				R.id.text1, spotNames));
-    	
-    }
+	
+
 }
---
+++ src/com/kervinramen/myspotfinder/placeslist.java
@@ -0,0 +1,71 @@
+package com.kervinramen.myspotfinder;
+
+
+
+
+
+
+import com.kervinramen.myspotfinder.model.Spots;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class placeslist extends ListActivity {
+	
+	public static final String KEY_USERNAME = "username";
+
+	String userName;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        if (userName == null) {
+            Bundle extras = getIntent().getExtras();
+            userName = extras != null ? extras.getString(KEY_USERNAME)
+                                    : null;
+        }
+        
+        
+        listPlaces();
+    }
+    
+    @Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		
+		//on click, open Google Maps Activity
+		Intent i = new Intent(this,  placesmap.class);
+		startActivity(i);
+	}
+    
+    
+    private void listPlaces()
+    {
+    	
+    	  // mRowId = (savedInstanceState == null) ? null :
+            //   (Long) savedInstanceState.getSerializable(SQLDbAdapter.KEY_ROWID);
+           
+	    	//getPlaces
+           	//TODO: Send username to Spots
+	    	Spots spots = Spots.getSpotsFromWS();
+	     
+	    	String[] spotNames = new String[spots.getSpots().size()];
+	    	
+	    	
+	    	for (int i = 0; i < spots.getSpots().size(); i++)
+	    	{
+	    		spotNames[i] = spots.getSpots().get(i).getName();
+	    	}
+	    	
+	       	//display in list view 	
+	    	setListAdapter(new ArrayAdapter<String>(this, R.layout.place_row,
+					R.id.text1, spotNames));
+    	
+    }
+}
